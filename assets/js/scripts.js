485e850f871b5abfe82679b5a696f03ebe39a485
Max Boog <max_boog@hotmail.com>
2021-02-28 15:47:03 +0100

    1.0.0

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,41 +1,24 @@
-// hamburger icon animation
 
-const toggle = document.getElementById('toggle');
-const nav = document.getElementById('nav');
-
-toggle.addEventListener('click', () => {
-	nav.classList.toggle('active');
-});
-
-$(document).on("click", "nav", function (e) {
-	if ($(e.target).is("a")) {
-		nav.classList.toggle('active')
+$(document).on("click", ".navbar-collapse.in", function (e) {
+	if ($(e.target).is("a") && $(e.target).attr("class") != "dropdown-toggle") {
+		$(this).collapse("hide");
 	}
 });
 
-// get in touch social button
+// function to make navbar smaller when scrolling
 
-const floating_btn = document.querySelector('.floating-btn');
-const social_panel_container = document.querySelector('.social-panel-container');
-
-floating_btn.addEventListener('click', () => {
-	social_panel_container.classList.toggle('visible')
+$(window).scroll(function () {
+	if ($(document).scrollTop() > 50) {
+		$("nav").addClass("shrink");
+	} else {
+		$("nav").removeClass("shrink");
+	}
 });
-
-
-// particles
-
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', '/assets/particles.json', function() {
-	console.log('callback - particles.js config loaded');
-  });
-
 
 // jquery easing
 
-jQuery.easing.jswing=jQuery.easing.swing,jQuery.extend(jQuery.easing,{def:"easeOutQuad",easeOutExpo:function(e,n,u,a,s){return n==s?u+a:a*(1-Math.pow(2,-10*n/s))+u}});
-
 $(document).ready(function () {
+	$("body").scrollspy({ target: ".navbar", offset: 70 });
 
 	$("a").on("click", function (event) {
 		if (this.hash !== "") {
